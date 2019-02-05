7dd253062add476ecb3fd36cd573d80ff2a56946
chuka okonkwo <okonkwo.henry2012@gmail.com>
2019-02-05 02:22:21 +0100

    ft #163634295 Admin can fetch a specific political office

--- app/routes/offices.js
+++ app/routes/offices.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/offices', officeController.createOffice);
 router.get('/offices', officeController.getOffices);
+router.get('/offices/:officeId', officeController.getOffice);
 
 
 export default router;
